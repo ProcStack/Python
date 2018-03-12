cb345bde6192b05b03add49b9799327f3fee2f6c
Kevin / Trancor <trancor@metal-asylum.net>
2018-03-12 03:24:29 -0400

    v0.0.6
    Updates; March 11, 2018 -- v0.0.6
    -'Left' key loads previous image in Directory.
    -'Right' key loads next image in Directory.
    -Right click zooming too quickly returned NaN and would break zoom.  It corrects for NaN now.
    -'F' now toggles full screen image view. 
    -Sped up load time of window.
    -"Image Info" menu added to display core data from image; name, res, file size
    -"Help" menu added to display keyboard shortcuts; non-active items.

--- pxlViewer/pxlViewer.py
+++ pxlViewer/pxlViewer.py
@@ -1,19 +1,33 @@
-############################################
-## pxlViewer v0.0.5                       ##
-## Image Viewer                           ##
-##  Written by Kevin Edzenga; ~2017       ##
-##   http://Metal-Asylum.net              ##
-##                                        ##
-## For aditional work, see my github-     ##
-##  https://github.com/procstack          ##
-############################################
+################################################
+## pxlViewer v0.0.6                           ##
+## Image Viewer                               ##
+##  Written by Kevin Edzenga; ~2017;2018      ##
+##   http://Metal-Asylum.net                  ##
+##                                            ##
+## Updates; March 11, 2018 -- v0.0.6          ##
+##  'Left' key loads previous image in Dir.   ##
+##  'Right' key loads next image in Dir.      ##
+##  Right click zooming too quickly returned  ##
+##    NaN and would break zoom.               ##
+##    It corrects for NaN now.                ##
+##  'F' now toggles full screen image view.   ##
+##  Sped up load time of window.              ##
+##  "Image Info" menu added to display core   ##
+##    data from image; name, res, file size   ##
+##  "Help" menu added to display keyboard     ##
+##    shortcuts; non-active items.            ##
+##                                            ##
+## For aditional work, see my github-         ##
+##  https://github.com/procstack              ##
+################################################
 
 """
  Learning should be a shared experiance
 """
 
 scriptNameText="pxlViewer"
-viewVersion="v0.0.5"
+viewVersion="v0.0.6"
+pxlViewerTitleBase=scriptNameText+" "+str(viewVersion)+" - "
 
 import zlib
 import bz2
@@ -37,9 +51,18 @@ else:
 curDir=os.getcwd()
 
 verbose=0
-if sys.argv[1] in ["-v","-verb", "-verbose"]:
-	verbose=1
-	
+loadImgUrl=''
+
+for arg in sys.argv:
+	if arg in ["-v","-verb", "-verbose"]:
+		verbose=1
+	else:
+		#Dunno when this would ever NEED to be checked, but just to be safe
+		if arg == bundleDir or "/".join(arg.split("/")[:-1]) == bundleDir:
+			loadImgUrl=''
+		else:
+			loadImgUrl=arg
+
 if verbose == 0:
 	# Fork the python session and close the parent session
 	# Keep terminal active for later usage
@@ -52,14 +75,14 @@ if verbose == 0:
 class ImageProcessor(QtGui.QMainWindow):
 	def __init__(self, parent=None):
 		super(ImageProcessor,self).__init__(parent)
-		global viewVersion
+		global pxlViewerTitleBase
+		global loadImgUrl
 		global sW
 		global sH
 		global scriptNameText
-		versionText=scriptNameText+" - "+str(viewVersion)
-		self.setWindowTitle(versionText)
-		self.winSize=[720,405]
-		self.setMinimumSize(self.winSize[0],self.winSize[1])
+		self.setWindowTitle(pxlViewerTitleBase)
+		self.winSize=[sW*.75,sH*.75]
+		self.setMinimumSize(self.winSize[0]*.2,self.winSize[1]*.2)
 		self.resize(self.winSize[0],self.winSize[1])
 		#self.setStyleSheet("padding:0px;")	
 		
@@ -72,9 +95,11 @@ class ImageProcessor(QtGui.QMainWindow):
 		self.curEntryObj=-1
 		self.dirImageList={}
 		self.loadIndexList=[]
+		self.loadNameList=[]
 		self.loadScrollList=[]
 		self.scrollIndexVal=0
 		self.scrollIndexHeight=-1
+		self.editViewWindow=None
 		self.mainWidget=QtGui.QWidget(self)
 		
 		
@@ -114,9 +139,8 @@ class ImageProcessor(QtGui.QMainWindow):
 		self.setCentralWidget(self.mainWidget)
 
 		## GUI COMPLETE ##
-
 		# Load image
-		self.loadAndScanDir(curDir+"/"+sys.argv[-1])
+		self.loadAndScanDir(curDir+"/", loadImgUrl)
 	def menuBarVis(self,tgl):
 		if tgl == 1:
 			### Menu Bar ###
@@ -140,10 +164,92 @@ class ImageProcessor(QtGui.QMainWindow):
 				#self.infoMenu.addAction(helpItem)
 				# Status Bar
 				#self.statusBar=self.statusBar()
+				
+				## Info Menu 
+				self.infoMenu=self.menuBar.addMenu('Image Info')
+				self.infoMenuItem_name=QtGui.QAction("name",self)
+				self.infoMenuItem_res=QtGui.QAction("res",self)
+				self.infoMenuItem_size=QtGui.QAction("size",self)
+				#infoItem.triggered.connect(partial(self.menuCommand, "toggleInfoWindow"))
+				self.infoMenu.addAction(self.infoMenuItem_name)
+				self.infoMenu.addAction(self.infoMenuItem_res)
+				self.infoMenu.addAction(self.infoMenuItem_size)
+				
+				self.updateImageInfoMenu(self.curEntryObj)
+				
+				## Help Menu
+				## This is annoying, can't seem to get the commands to run without faulting on menu generation
+				helpMenu=self.menuBar.addMenu('Help')
+				
+				helpText="Space to reset image zoom/position."
+				helpItem=QtGui.QAction(helpText,self)
+				#helpItem.triggered.connect(self.editViewWindow.runCommand("resize(0);"))
+				helpMenu.addAction(helpItem)
+				
+				helpText="R to reload image."
+				helpItem=QtGui.QAction(helpText,self)
+				helpMenu.addAction(helpItem)
+				#helpItem.triggered.connect(self.editViewWindow.runCommand("refreshImage();"))
+				
+				helpText="F to enter full screen."
+				helpItem=QtGui.QAction(helpText,self)
+				helpMenu.addAction(helpItem)
+				#helpItem.triggered.connect(self.menuBar.setVisible(0))
+				
+				helpText="Alt to toggle menuBar visibility."
+				helpItem=QtGui.QAction(helpText,self)
+				#helpItem.triggered.connect(self.editViewWindow.toggleMenuBar())
+				helpMenu.addAction(helpItem)
+				
+				helpMenu.addSeparator()
+						
+				helpText="Left key loads Previous image."
+				helpItem=QtGui.QAction(helpText,self)
+				#helpItem.triggered.connect(self.prevImage())
+				helpMenu.addAction(helpItem)
+				
+				helpText="Right key loads Next image."
+				helpItem=QtGui.QAction(helpText,self)
+				#helpItem.triggered.connect(self.nextImage())
+				helpMenu.addAction(helpItem)
+				"""
+				helpList=[]
+				helpFuncList=[]
+				helpList.append("Space to reset image zoom/position.")
+				helpFuncList.append("resize(0);")
+				helpList.append("R to reload image.")
+				helpFuncList.append(None)
+				helpList.append("F to enter full screen.")
+				helpFuncList.append("self.menuBar.setVisible(0)")
+				helpList.append("Alt to toggle menuBar visibility.")
+	
+				helpFuncList.append("self.editViewWindow.toggleMenuBar()")
+				helpList.append("sep")
+				helpFuncList.append(None)
+				helpList.append("Left key loads Previous image.")
+				helpFuncList.append("self.prevImage()")
+				helpList.append("Right key loads Next image.")
+				helpFuncList.append("self.nextImage()")
+				for x,h in enumerate(helpList):
+					if h == "sep":
+						helpMenu.addSeparator()
+					else:
+						helpItem=QtGui.QAction(h,self)
+						##if helpFuncList[x] != None:
+						##	helpItem.triggered.connect(partial("self.runCommand", helpFuncList[x]))
+						helpMenu.addAction(helpItem)
+				"""
 			except:
 				self.menuBar.setVisible(1);
 		else:
 			self.menuBar.setVisible(0);
+	def runCommand(self,cmd):
+		print cmd
+	def fullScreenVis(self,tgl):
+		if tgl == 1:
+			self.showFullScreen()
+		else:
+			self.showNormal()
 	def setCursorPointing(self):
 		QtGui.QWidget.setCursor(self.infoMenu,QtCore.Qt.PointingHandCursor)
 	def setCursorArrow(self):
@@ -184,9 +290,9 @@ class ImageProcessor(QtGui.QMainWindow):
 		QScrollBar:vertical {width:10px;color:#ffffff;background-color:#808080;border:1px solid #202020;}
 		QStatusBar {color:#ffffff;background-color:#606060;border:1px solid #202020;}"""
 		self.setStyleSheet(styleSheetCss)
-	def loadAndScanDir(self, imagePath):
+	def loadAndScanDir(self, imagePath, imageNameRel):
 		if imagePath != "":
-			extList=["jpg","jpeg","gif","png"]
+			extList=["jpg","jpeg","gif","png", "bmp", "svg"] # Check SVG, since it likes vector postions
 			activeList=[]
 			if os.path.isdir(imagePath):
 				#Don't think this is working
@@ -301,31 +407,40 @@ class ImageProcessor(QtGui.QMainWindow):
 				scrollOffset=0
 				scrollAdd=0
 				self.loadIndexList=[]
+				self.loadNameList=[]
 				self.loadScrollList=[]
 				loadObj=-1
+				self.curListIndex=-1
 				for x,p in enumerate(activeList):
 					scrollOffset+=scrollAdd+pad
-					
 					#IndexImageEntry(parent, index, name, path, scaleSize)
 					curImg=IndexImageEntry(self,x,p,imagePath+"/",size)
 					self.curImgListBlock.addWidget(curImg)
 					curImg.offset=scrollOffset
 					self.loadIndexList.append(curImg)
+					self.loadNameList.append(p)
 					self.loadScrollList.append([])
 					self.loadScrollList[-1].append(scrollOffset)
 					self.loadScrollList[-1].append(scrollOffset+curImg.imgSizeIndexList[1])
 					scrollAdd=curImg.imgSizeIndexList[1]
-					if loadObj==-1:
+					if (loadObj==-1 and imageName=='') or p==imageNameRel:
 						loadObj=curImg
+						self.curListIndex=x
 				self.scrollIndexBlock.setWidget(scrollInner)
 				#entryBlock.addWidget(self.scrollIndexBlock)
 				self.imageDisplayBlock.addLayout(entryBlock)
-				self.updateScrollIndex()
+				#self.updateScrollIndex() ## Load thumbnails of images in the gallery viewer
 				self.loadImageEntry(loadObj)
+				self.updateImageInfoMenu(loadObj)
 	def setOutputDir(self):
 		folderPicker=QtGui.QFileDialog.getExistingDirectory(self,"Set Output Directory")
 		if folderPicker != "":
 			self.outDirField.setText(folderPicker)
+	## updateScrollIndex; currently useless; scrollable directory view turned off right now
+	## This will be turned on once multi-threading is implimented.
+	## Currently, this slows down launching the window a fair bit.
+	## Going to set it up to monitor load status, then show up as a selectable scrollable gallery.
+	## Dynamically loading images along the way.
 	def updateScrollIndex(self):
 		self.scrollIndexVal=self.scrollIndexBlock.verticalScrollBar().value()
 		self.scrollIndexHeight=self.scrollIndexBlock.size().height()
@@ -338,11 +453,12 @@ class ImageProcessor(QtGui.QMainWindow):
 					self.loadIndexList[x].loadImage()
 					popList.append(x)
 			if len(popList) > 0:
-				for p in range(len(popList)): # Delete backwards to no delete the wrong index, since everything falls back 1 on delete
+				for p in range(len(popList)): # Delete backwards to not delete the wrong index, since everything falls back 1 on delete
 					rp=len(popList)-1-p
 					del self.loadIndexList[rp]
 					del self.loadScrollList[rp]
-	def loadImageEntry(self,obj):
+	def loadImageEntry(self,obj):  # THIS ONLY RUNS ON LAUNCH, NEVER AGAIN
+		global pxlViewerTitleBase
 		self.clearLayout(self.curImageSettings)
 		
 		### SETTINGS ###
@@ -359,11 +475,48 @@ class ImageProcessor(QtGui.QMainWindow):
 		self.curEntryBlock.setMargin(0)
 		pol=QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
 		self.editViewWindow.setSizePolicy(pol)
+		self.editViewWindow.setFocus()
+		versionText=pxlViewerTitleBase+str(obj.imgName)
+		self.setWindowTitle(versionText)
+		self.updateImageInfoMenu(obj)
+	def updateImageInfoMenu(self, loadImage): ## Update Image Info menu pulldown
+		try:
+			self.infoMenuItem_name.setText(loadImage.imgName)
+			self.infoMenuItem_res.setText(str(loadImage.imgSize[0])+" x "+str(loadImage.imgSize[1]))
+			fileSize=str(loadImage.fileSize/1024.0).split(".")
+			fileSize[1]=fileSize[1][0:2]
+			fileSize=".".join(fileSize)
+			self.infoMenuItem_size.setText(fileSize+" kb")
+		except:
+			pass;
+	def prevImage(self): ## Accessed through EntryViewer
+		global pxlViewerTitleBase
+		self.curListIndex=self.curListIndex-1 if (self.curListIndex-1)>=0 else len(self.loadIndexList)-1
+		loadImage=self.loadIndexList[self.curListIndex]
+		self.curEntryObj=loadImage
+		w=loadImage.imgSize[0]
+		h=loadImage.imgSize[1]
+		self.editViewWindow.setImage(loadImage.imgName,w,h)
+		self.updateImageInfoMenu(loadImage)
+		versionText=pxlViewerTitleBase+str(loadImage.imgName)
+		self.setWindowTitle(versionText)
+	def nextImage(self): ## Accessed through EntryViewer
+		global pxlViewerTitleBase
+		self.curListIndex=self.curListIndex+1 if (self.curListIndex+1)!=len(self.loadIndexList) else 0
+		loadImage=self.loadIndexList[self.curListIndex]
+		self.curEntryObj=loadImage
+		w=loadImage.imgSize[0]
+		h=loadImage.imgSize[1]
+		self.editViewWindow.setImage(loadImage.imgName,w,h)
+		self.updateImageInfoMenu(loadImage)
+		versionText=pxlViewerTitleBase+str(loadImage.imgName)
+		self.setWindowTitle(versionText)
+		
 	def menuCommand(self, cmd):
 		self.editViewWindow.runCommand(cmd)
 	def updateGalleryVariables(self):
 		self.galleryName=self.globalGalleryName.text()
-	def clearLayout(self, layout):
+	def clearLayout(self, layout): ## Remove all children layouts and widgets
 		children=[]
 		postRemoveItem=[]
 		for l in range(layout.count()):
@@ -379,21 +532,13 @@ class ImageProcessor(QtGui.QMainWindow):
 		if len(postRemoveItem) > 0: # Removing spacers in the loop will skip widgets after that point in the array
 			for i in postRemoveItem: # So delete them after the fact
 				layout.removeItem(i)
-	"""
-	def mousePressEvent(self, e):
-		print "click"
-	def mouseReleaseEvent(self, e):
-		print "release"
-	def mouseMoveEvent(self, e):
-		print "move"
-	"""
 	def keyPressEvent(self, e):
 		global verbose
 		if verbose:
 			print e
 		if e.key()==QtCore.Qt.Key_Escape:
 			self.quitApp()
-	def progressBar(self,init):
+	def progressBar(self,init): ## Needless, leaving for future functionality
 		if init == -1:
 			val=0
 		elif init == -2:
@@ -401,9 +546,9 @@ class ImageProcessor(QtGui.QMainWindow):
 		elif init >= 0:
 			val=0
 		return None
-				
-	def resizeEvent(self, e):
-		return None
+	def resizeEvent(self, e): ## Resize image to screen, correct for resizing disparity
+		if self.editViewWindow != None:
+			self.editViewWindow.runCommand("resize(0)")
 	def quitApp(self):
 		QtGui.qApp.quit()
 class IndexImageEntry(QtGui.QWidget): #Individual indexList image entries
@@ -415,7 +560,9 @@ class IndexImageEntry(QtGui.QWidget): #Individual indexList image entries
 		self.imgFolder=path
 		self.imgPath=path+name # Path to image on disk
 		self.loaded=0 # Current state, reading from disk, keeps ram usage and load time lower
-		
+
+		self.fileSize=os.path.getsize(path+"/"+name)
+
 		self.imgSize=[-1,-1] # Disk image size
 		self.imgSizeFull=[-1,-1] # Full size image for web
 		self.imgSizeFullTile=[-1,-1] # Tile array sizes for web, [x1,y1, x2,y2, x3,y3, ... xn,yn]
@@ -474,6 +621,7 @@ class EntryViewer(QtWebKit.QWebView):
 	def __init__(self, win, entryObj):
 		QtWebKit.QWebView.__init__(self)
 		global bundleDir
+		global verbose
 		self.entry=entryObj
 		self.win=win
 		self.offset=[]
@@ -481,7 +629,9 @@ class EntryViewer(QtWebKit.QWebView):
 		self.mouseDrag=0
 		self.verbose=0
 		self.menuVis=0
-		if sys.argv[1] in ["-v","-verb", "-verbose", "-d", "-debug"]:
+		self.fullScreenVis=0
+		self.overMenuBar=0
+		if verbose:
 			self.verbose=1
 		self.settings().setAttribute(QtWebKit.QWebSettings.JavascriptEnabled, True)
 		self.settings().setUserStyleSheetUrl(QtCore.QUrl.fromLocalFile(bundleDir+"/style.css"))
@@ -495,15 +645,23 @@ class EntryViewer(QtWebKit.QWebView):
 		self.setHtml(self.src,baseUrl=QtCore.QUrl().fromLocalFile(self.entry.imgFolder))
 
 		self.page().mainFrame().addToJavaScriptWindowObject("opWin",self)
-        
+		
 	def setImage(self,img,w,h):
-		super(EntryViewer,self).page().mainFrame().evaluateJavaScript("setEntryImage('"+str(img)+"',"+str(w)+","+str(h)+");");
+		super(EntryViewer,self).page().mainFrame().evaluateJavaScript("setEntryImage('"+str(img)+"',"+str(w)+","+str(h)+");")
 	def setText(self,text):
-		super(EntryViewer,self).page().mainFrame().evaluateJavaScript("setEntryText('"+str(text)+"');");
+		super(EntryViewer,self).page().mainFrame().evaluateJavaScript("setEntryText('"+str(text)+"');")
 	def updateVariable(self,var,val):
-		super(EntryViewer,self).page().mainFrame().evaluateJavaScript(str(var)+"="+str(val)+";");
+		super(EntryViewer,self).page().mainFrame().evaluateJavaScript(str(var)+"="+str(val)+";")
 	def runCommand(self,cmd):
-		super(EntryViewer,self).page().mainFrame().evaluateJavaScript(cmd+";");
+		super(EntryViewer,self).page().mainFrame().evaluateJavaScript(cmd+";")
+	def loadFinished(self, ok):
+		self.runCommand("resize(0)")
+	def toggleMenuBar(self):
+			self.menuVis=(self.menuVis+1)%2
+			self.win.menuBarVis(self.menuVis)
+	def toggleFullScreen(self):
+			self.fullScreenVis=(self.fullScreenVis+1)%2
+			self.win.fullScreenVis(self.fullScreenVis)
 	def keyPressEvent(self, e):
 		if e.key()==QtCore.Qt.Key_Escape:
 			self.win.quitApp()
@@ -522,25 +680,29 @@ class EntryViewer(QtWebKit.QWebView):
 		
 	def mousePressEvent(self, e):
 		self.offset=e.pos()
-		self.mouseDown=e.button()
-		self.updateVariable("mouseX",self.offset.x())
-		self.updateVariable("mouseY",self.offset.y())
-		self.updateVariable("origMouseX",self.offset.x())
-		self.updateVariable("origMouseY",self.offset.y())
-		#super(EntryViewer,self).page().mainFrame().evaluateJavaScript("writeEntryText('[ "+str(self.offset.x())+", "+str(self.offset.y())+" ]');");
+		if self.overMenuBar==1:
+			self.toggleMenuBar()
+			self.overMenuBar=0
+			self.runCommand("menuBarStatus(-1)")
+		else:
+			self.mouseDown=e.button()
+			self.updateVariable("mouseX",self.offset.x())
+			self.updateVariable("mouseY",self.offset.y())
+			self.updateVariable("origMouseX",self.offset.x())
+			self.updateVariable("origMouseY",self.offset.y())
+			#super(EntryViewer,self).page().mainFrame().evaluateJavaScript("writeEntryText('[ "+str(self.offset.x())+", "+str(self.offset.y())+" ]');");
 	def mouseReleaseEvent(self, e):
 		self.mouseDown=0
 		super(EntryViewer,self).page().mainFrame().evaluateJavaScript("endDrag();dragging=0;");
 		self.updateVariable("dragCount",0)
 		self.updateVariable("mButton",-1)
 	def mouseMoveEvent(self, e):
-		if type(self.offset)!=QtCore.QPoint:
-			self.offset=e.pos();
+		#if type(self.offset)!=QtCore.QPoint:
+		self.offset=e.pos();
 		if self.mouseDown>0:
-			self.offset=e.pos();
+			self.updateVariable("dragging",1)
 			self.updateVariable("mouseX",self.offset.x())
 			self.updateVariable("mouseY",self.offset.y())
-			self.updateVariable("dragging",1)
 			self.updateVariable("mButton",self.mouseDown)
 			if self.mouseDrag==0:
 				super(EntryViewer,self).page().mainFrame().evaluateJavaScript("startDrag();");
@@ -548,6 +710,13 @@ class EntryViewer(QtWebKit.QWebView):
 			self.mouseDrag+=1
 		else:
 			self.mouseDrag=0
+			mouseY=self.offset.y()
+			if mouseY<20:
+				self.overMenuBar=1
+				self.runCommand("menuBarStatus("+str(int(mouseY))+")")
+			elif mouseY>20 and self.overMenuBar>0:
+				self.overMenuBar=0
+				self.runCommand("menuBarStatus(-1)")
 	@QtCore.pyqtSlot(str)
 	def varValue(self, varArray):
 		print varArray
@@ -557,8 +726,13 @@ class EntryViewer(QtWebKit.QWebView):
 			if self.verbose==0:
 				self.runCommand("noContextMenu()")
 		elif message == "toggleMenuBarVis":
-			self.menuVis=(self.menuVis+1)%2
-			self.win.menuBarVis(self.menuVis)
+			self.toggleMenuBar()
+		elif message == "toggleFullScreen":
+			self.toggleFullScreen()
+		elif message == "Left":
+			self.win.prevImage()
+		elif message == "Right":
+			self.win.nextImage()
 		else:
 			print message
 if __name__ == '__main__':
